8764d23d4100bb3de92eaa3b102adee17b10d0de
moannuo <ademenet@e3r9p14.42.fr>
2017-12-14 16:41:57 +0100

    Update

--- stock_price_prediction/utils.py
+++ stock_price_prediction/utils.py
@@ -1,43 +1,81 @@
-"""
-Some magics for stock price prediction notebook.
-"""
+import time
+import warnings
 import numpy as np
+from numpy import newaxis
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+import matplotlib.pyplot as plt
 
-def normalize(seq_arr):
-    """Normalize sequential datas.
+warnings.filterwarnings("ignore")
 
-    Formula:
-        $$ n_i = (\frac{p_i}{p_0}) - 1 $$
+
+def plot_losses(history):
+    """Plot the loss and validation loss.
     """
-    norm_arr = list()
-    for seq in seq_arr:
-        norm_seq = [((float(x) / seq[0])- 1) for x in seq]
-        norm_arr.append(norm_seq)
-    return norm_arr
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
 
-def data_processing(datas, seq_len=50):
-    """Pre-process the datas.
 
-    Inputs:
-        datas (np.array): the datas to be processed.
-    """
-    cuts = round(len(datas) / seq_len)
-    processed = list()
+def plot_results_multiple(predicted_data, true_data, prediction_len):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    for i, data in enumerate(predicted_data):
+        padding = [None for p in range(i * prediction_len)]
+        plt.plot(padding + data, label='Prediction')
+        plt.legend()
+    plt.show()
 
-    for index in range(cuts):
-        processed.append(datas[index: index + (seq_len + 1)])
+
+def load_data(filename, seq_len, normalise_window):
+    f = open(filename, 'r').read()
+    data = f.split('\n')
+    sequence_length = seq_len + 1
+    result = []
+    for index in range(len(data) - sequence_length):
+        result.append(data[index: index + sequence_length])
     
-    processed = np.array(normalize(processed))
-    train_boundary = round(0.7 * processed.shape[0])
-    train = processed[:int(train_boundary), :]
+    if normalise_window:
+        result = normalise_windows(result)
+
+    result = np.array(result)
+
+    row = round(0.9 * result.shape[0])
+    train = result[:int(row), :]
     np.random.shuffle(train)
-
-    X_train = train[:, :-1]
+    x_train = train[:, :-1]
     y_train = train[:, -1]
-    X_test = processed[int(train_boundary):, :-1]
-    y_test = processed[int(train_boundary):, -1]
+    x_test = result[int(row):, :-1]
+    y_test = result[int(row):, -1]
 
-    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))  
 
-    return [X_train, y_train, X_test, y_test]
+    return [x_train, y_train, x_test, y_test]
+
+
+def normalise_windows(window_data):
+    normalised_data = []
+    for window in window_data:
+        normalised_window = [((float(p) / float(window[0])) - 1) for p in window]
+        normalised_data.append(normalised_window)
+    return normalised_data
+
+
+def predict_sequences_multiple(model, data, window_size, prediction_len):
+    prediction_seqs = []
+    for i in range(int(len(data)/prediction_len)):
+        curr_frame = data[i*prediction_len]
+        predicted = []
+        for j in range(prediction_len):
+            predicted.append(model.predict(curr_frame[newaxis,:,:])[0,0])
+            curr_frame = curr_frame[1:]
+            curr_frame = np.insert(curr_frame, [window_size-1], predicted[-1], axis=0)
+        prediction_seqs.append(predicted)
+    return prediction_seqs
